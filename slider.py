180b98910921678e1c34d926bb4ef275b19678b8
kvabapo <36245725+kvabapo@users.noreply.github.com>
2018-03-20 14:37:38 +0800

    Update slider.py
    updated CSS locator

--- slider.py
+++ slider.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Firefox()
 driver.get("http://seiyria.com/bootstrap-slider/")
-source_element = driver.find_element_by_class_name('slider-track')
-slider = driver.find_element_by_css_selector(".round[style='left: 76%;']")
+slider = driver.find_element_by_css_selector("div#example-1 div.slider-handle.min-slider-handle.round")
 move = ActionChains(driver)
 move.click_and_hold(slider).move_by_offset(40, 0).release().perform()
 driver.close()
